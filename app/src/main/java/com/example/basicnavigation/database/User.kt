a5267d03c5cd8982e89fb86472d14e5697dbcd00
Rave7v <fernando080499@hotmail.com>
2021-11-24 14:03:41 -0600

    clase del 34

---
+++ app/src/main/java/com/example/basicnavigation/database/User.kt
@@ -0,0 +1,6 @@
+package com.example.basicnavigation.database
+
+class  User(username: String,id: String){
+    val username =username
+    val id = id
+}
--- app/src/main/java/fragments/destinationFragment.kt
+++ app/src/main/java/fragments/destinationFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.basicnavigation.R
 import com.example.basicnavigation.databinding.FragmentDestinationBinding
+import com.example.basicnavigation.database.User
 import com.example.basicnavigation.databinding.FragmentLeftBinding
 
 class destinationFragment : Fragment() {
+
     private lateinit var binding: FragmentDestinationBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +27,16 @@ class destinationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val receivedUsername = arguments?.getString("username_arg")
         binding.tvReceivedArg.setText(receivedUsername)
+        binding.rvUserEntries.layoutManager = LinearLayoutManager(view?.context)
+        val adapter = destinationAdapter(generateData())
+        binding.rvUserEntries.adapter = adapter
+    }
+
+    fun generateData():List<User>{
+        var users = mutableListOf<User>()
+        for (x in 1..10){
+            users.add(User("user${x}","$x"))
+        }
+        return users
     }
 }
